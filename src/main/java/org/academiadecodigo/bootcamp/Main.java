95d9c16561cb14bdcb6b4cf4319e259dc52b097d
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-07-27 16:04:21 +0100

    skelleton

---
+++ src/main/java/org/academiadecodigo/bootcamp/Main.java
@@ -0,0 +1,28 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 27/07/2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
+
---
+++ src/main/java/org/academiadecodigo/bootcamp/model/Answer.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp.model;
+
+/**
+ * Created by codecadet on 27/07/2017.
+ */
+public class Answer {
+}
