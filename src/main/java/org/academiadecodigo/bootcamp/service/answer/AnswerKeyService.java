3d6895de9dd95e7f17037c45d24770fc4049e3f8
Manuel Pedro <mbpedro1997@gmail.com>
2017-07-27 17:49:21 +0100

    trasaction update

--- src/main/java/org/academiadecodigo/bootcamp/service/answer/AnswerKeyService.java
+++ src/main/java/org/academiadecodigo/bootcamp/service/answer/AnswerKeyService.java
@@ -4,6 +4,8 @@ import org.academiadecodigo.bootcamp.model.Answer;
 import org.academiadecodigo.bootcamp.model.KeyWord;
 import org.academiadecodigo.bootcamp.model.dao.AnswerDao;
 import org.academiadecodigo.bootcamp.model.dao.KeyWordDao;
+import org.academiadecodigo.bootcamp.persistence.TransactionException;
+import org.academiadecodigo.bootcamp.persistence.TransactionManager;
 import org.academiadecodigo.bootcamp.utils.Random;
 
 import java.util.ArrayList;
@@ -16,38 +18,53 @@ public class AnswerKeyService {
 
     private KeyWordDao keyDao;
     private AnswerDao answerDao;
+    private TransactionManager manager;
 
-    public AnswerKeyService(KeyWordDao keyDao, AnswerDao answerDao) {
+    public AnswerKeyService(KeyWordDao keyDao, AnswerDao answerDao, TransactionManager manager) {
+        this.manager = manager;
         this.keyDao = keyDao;
         this.answerDao = answerDao;
     }
 
     public String getAnswer(String question) {
 
+        try {
 
-        String[] words = question.split("[ ,.?!'\";:\\-+*/()$£@\n\r]");
 
-        KeyWord keyWord = null;
-        List<KeyWord> keyWords = new ArrayList<>();
+            manager.beginTransaction();
 
-        for (String word : words) {
+            String[] words = question.split("[ ,.?!'\";:\\-+*/()$£@\n\r]");
 
-            keyWord = keyDao.findByWord(word);
+            KeyWord keyWord = null;
+            List<KeyWord> keyWords = new ArrayList<>();
 
-            if (keyWord != null) {
-                keyWords.add(keyWord);
+            for (String word : words) {
+
+                keyWord = keyDao.findByWord(word);
+
+                if (keyWord != null) {
+                    keyWords.add(keyWord);
+                }
             }
+
+            if (keyWord == null) {
+                //return default;
+            }
+
+
+            keyWord = keyWords.get(Random.MathRandom(0, keyWords.size()));
+            List<Answer> answers = answerDao.findByKeyId(keyWord.getId());
+            String answer = answers.get(Random.MathRandom(0, answers.size())).getSentence();
+
+            manager.commitTransaction();
+
+            return answer;
+
+        }catch (TransactionException ex){
+            manager.rollbackTransaction();
+            System.err.println("Error: " + ex.getMessage());
         }
 
-        if (keyWord == null) {
-            //return default;
-        }
-
-
-        keyWord = keyWords.get(Random.MathRandom(0, keyWords.size()));
-        List<Answer> answers = answerDao.findByKeyId(keyWord.getId());
-        String answer = answers.get(Random.MathRandom(0, answers.size())).getSentence();
-
-        return answer;
+        return null;
     }
 }
