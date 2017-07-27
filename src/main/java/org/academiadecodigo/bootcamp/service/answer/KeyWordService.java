72c06afeb426e8a0e88a0bb01268cee621cd9a65
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-07-27 20:20:03 +0100

    erase default

---
+++ src/main/java/org/academiadecodigo/bootcamp/service/answer/KeyWordService.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.bootcamp.service.answer;
+
+import org.academiadecodigo.bootcamp.model.dao.KeyWordDao;
+import org.academiadecodigo.bootcamp.persistence.TransactionManager;
+
+/**
+ * Created by codecadet on 27/07/2017.
+ */
+public class KeyWordService {
+
+    private KeyWordDao keyWordDao;
+    private TransactionManager transactionManager;
+
+
+
+
+
+
+
+}
