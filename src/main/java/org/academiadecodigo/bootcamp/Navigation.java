4521eaad38cd8535d562b2845246b85600a09681
Manuel Pedro <mbpedro1997@gmail.com>
2017-07-27 18:19:06 +0100

    service update

--- src/main/java/org/academiadecodigo/bootcamp/Navigation.java
+++ src/main/java/org/academiadecodigo/bootcamp/Navigation.java
@@ -1,7 +1,117 @@
 package org.academiadecodigo.bootcamp;
 
-/**
- * Created by codecadet on 27/07/2017.
- */
-public class Navigation {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.academiadecodigo.bootcamp.controller.Controller;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public final class Navigation {
+
+    private static final String VIEW_PATH = "/View";
+    // static instance of this class
+    private static Navigation instance = null;
+
+    // ... singleton stuff...
+
+    private final int MIN_WIDTH = 1024; // window width
+    private final int MIN_HEIGHT = 768; // window height
+
+    private LinkedList<Scene> scenes = new LinkedList<Scene>(); // Navigation History
+    private Controller controller;
+    private Stage stage; // reference to the application window
+
+    // ... navigation stuff ...
+
+    // private constructor so it's not possible to instantiate from outside
+    private Navigation() {
+    }
+
+    // static method that returns the instance
+    public static synchronized Navigation getInstance() {
+
+        // the instance is created only the first time this method is called
+        if (instance == null) {
+            instance = new Navigation();
+        }
+
+        // it always returns the same instance, there is no way to have another one
+        return instance;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void loadScreen(String view) {
+        try {
+
+            // Instantiate the view and the controller
+            FXMLLoader fxmlLoader;
+            fxmlLoader = new FXMLLoader(getClass().getResource(VIEW_PATH + "/" + view + ".fxml"));
+            Parent root = fxmlLoader.load();
+            controller = fxmlLoader.getController();
+            // Create a new scene and add it to the stack
+            Scene scene = new Scene(root, MIN_WIDTH, MIN_HEIGHT);
+            scenes.push(scene);
+
+            // Put the scene on the stage
+            setScene(scene);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failure to load view " + view + " : " + e.getMessage());
+        }
+    }
+
+    private void setScene(Scene scene) {
+
+        // set the scene
+        stage.setScene(scene);
+
+        // show the stage to reload
+        stage.show();
+    }
+
+    public void back() {
+
+        if (scenes.isEmpty()) {
+            return;
+        }
+
+        // remove the current scene from the stack
+        scenes.pop();
+
+
+        // load the scene at the top of the stack
+        setScene(scenes.peek());
+    }
+
+    public Node getNode(String view) {
+
+        FXMLLoader fxmlLoader;
+        fxmlLoader = new FXMLLoader(getClass().getResource(VIEW_PATH + "/" + view + ".fxml"));
+
+
+        try {
+
+            Node node = fxmlLoader.load();
+
+            controller = fxmlLoader.getController();
+
+            return node;
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
+    public Controller getController() {
+        return controller;
+    }
 }
--- src/main/java/org/academiadecodigo/bootcamp/service/answer/AnswerKeyService.java
+++ src/main/java/org/academiadecodigo/bootcamp/service/answer/AnswerKeyService.java
@@ -26,7 +26,7 @@ public class AnswerKeyService {
         this.answerDao = answerDao;
     }
 
-    public String getAnswer(String question) {
+    public Answer getAnswer(String question) {
 
         try {
 
@@ -54,7 +54,7 @@ public class AnswerKeyService {
 
             keyWord = keyWords.get(Random.MathRandom(0, keyWords.size()));
             List<Answer> answers = answerDao.findByKeyId(keyWord.getId());
-            String answer = answers.get(Random.MathRandom(0, answers.size())).getSentence();
+            Answer answer = answers.get(Random.MathRandom(0, answers.size()));
 
             manager.commitTransaction();
 
