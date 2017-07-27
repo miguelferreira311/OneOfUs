b9ee31e832cee08d770138eae85c0845d0e53b85
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-07-27 16:55:10 +0100

    mapping answer keyword

--- src/main/java/org/academiadecodigo/bootcamp/model/Answer.java
+++ src/main/java/org/academiadecodigo/bootcamp/model/Answer.java
@@ -4,4 +4,41 @@ package org.academiadecodigo.bootcamp.model;
  * Created by codecadet on 27/07/2017.
  */
 public class Answer {
+
+
+    private int id;
+    private String sentence;
+    private KeyWord keyWordId;
+
+    public Answer() {
+    }
+
+    public Answer(String sentence, KeyWord keyWordId) {
+        this.sentence = sentence;
+        this.keyWordId = keyWordId;
+    }
+
+    public KeyWord getKeyWordId() {
+        return keyWordId;
+    }
+
+    public void setKeyWordId(KeyWord keyWordId) {
+        this.keyWordId = keyWordId;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/org/academiadecodigo/bootcamp/model/KeyWord.java
+++ src/main/java/org/academiadecodigo/bootcamp/model/KeyWord.java
@@ -4,4 +4,30 @@ package org.academiadecodigo.bootcamp.model;
  * Created by codecadet on 27/07/2017.
  */
 public class KeyWord {
+
+    private int id;
+    private String word;
+
+    public KeyWord() {
+    }
+
+    public KeyWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
