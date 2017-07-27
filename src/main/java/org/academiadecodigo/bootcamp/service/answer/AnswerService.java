31b3d417b0083533cf65343cb6531de1f94ba3b4
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-07-27 20:26:01 +0100

    answer and kw services

---
+++ src/main/java/org/academiadecodigo/bootcamp/service/answer/AnswerService.java
@@ -0,0 +1,32 @@
+package org.academiadecodigo.bootcamp.service.answer;
+
+import org.academiadecodigo.bootcamp.model.Answer;
+import org.academiadecodigo.bootcamp.model.dao.AnswerDao;
+import org.academiadecodigo.bootcamp.persistence.TransactionManager;
+
+import java.util.List;
+
+/**
+ * Created by codecadet on 27/07/2017.
+ */
+public class AnswerService {
+
+    private AnswerDao answerDao;
+    private TransactionManager transactionManager;
+
+    public void addAnswer(Answer answer){
+
+    }
+
+
+    public void removeAnswer(Answer answer){
+
+    }
+
+    public List<Answer> findAll(){
+
+        return null;
+    }
+
+
+}
--- src/main/java/org/academiadecodigo/bootcamp/service/answer/KeyWordService.java
+++ src/main/java/org/academiadecodigo/bootcamp/service/answer/KeyWordService.java
@@ -1,7 +1,10 @@
 package org.academiadecodigo.bootcamp.service.answer;
 
+import org.academiadecodigo.bootcamp.model.KeyWord;
 import org.academiadecodigo.bootcamp.model.dao.KeyWordDao;
 import org.academiadecodigo.bootcamp.persistence.TransactionManager;
+
+import java.util.List;
 
 /**
  * Created by codecadet on 27/07/2017.
@@ -12,7 +15,19 @@ public class KeyWordService {
     private TransactionManager transactionManager;
 
 
+    public void addAnswer(KeyWord word){
 
+    }
+
+
+    public void removeAnswer(KeyWord word){
+
+    }
+
+    public List<KeyWord> findAll(){
+
+        return null;
+    }
 
 
 
