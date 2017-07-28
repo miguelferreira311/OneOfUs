74f6af44fc11f26788f5728175418f7475e76f4a
Audrey Lopes <audrey_lopes@hotmail.com>
2017-07-28 09:21:47 +0100

    quotes \n

--- src/main/java/org/academiadecodigo/bootcamp/ServiceInjections.java
+++ src/main/java/org/academiadecodigo/bootcamp/ServiceInjections.java
@@ -91,12 +91,12 @@ public class ServiceInjections {
 
 
         quoteService.addQuote(new Quote("\"Falar é fácil. Mostra-me o código.\" - Linus Torvalds"));
-        quoteService.addQuote(new Quote("\"Não te preocupes se não funcionar corretamente. Se tudo funcionasse ficarias sem emprego.\""));
-        quoteService.addQuote(new Quote("\"Existem duas maneiras de construir um projeto de software. Uma maneira é tornar tão simples que, obviamente, não há deficiências. E o outro caminho é tornar tão complicado que não haja deficiências óbvias. \"- C.A.R. Hoare"));
-        quoteService.addQuote(new Quote("\"A maioria dos programadores bons programa não porque esperam receber pagamento ou reconhecimento pelo público, mas porque é divertido programar.\"- Linus Torvalds"));
+        quoteService.addQuote(new Quote("\"Não te preocupes se não funcionar corretamente.\n Se tudo funcionasse ficarias sem emprego.\""));
+        quoteService.addQuote(new Quote("\"Existem duas maneiras de construir um projeto de software.\n Uma maneira é tornar tão simples que, \n obviamente, não há deficiências. E o outro caminho \n é tornar tão complicado que não haja deficiências óbvias. \"\n- C.A.R. Hoare"));
+        quoteService.addQuote(new Quote("\"A maioria dos bons programadores programa, \n não porque esperam receber pagamento ou reconhecimento pelo público, \n mas porque é divertido programar.\"- Linus Torvalds"));
         quoteService.addQuote(new Quote("\"Iterar é humano, recursar é divino.\"- L. Peter Deutsch"));
-        quoteService.addQuote(new Quote("\"Às vezes vale a pena ficar na cama na segunda-feira, ao invés de passar o resto da semana a corrigir o código de segunda-feira\" - Christopher Thompson"));
-        quoteService.addQuote(new Quote("\"Em teoria, a teoria e a prática são a mesma coisa. Na prática não.\"- Yoggi Berra"));
+        quoteService.addQuote(new Quote("\"Às vezes vale a pena ficar na cama na segunda-feira, \n ao invés de passar o resto da semana a corrigir o código de segunda-feira\"\n - Christopher Thompson"));
+        quoteService.addQuote(new Quote("\"Em teoria, a teoria e a prática são a mesma coisa. \n Na prática não.\"- Yoggi Berra"));
 
 
     }
