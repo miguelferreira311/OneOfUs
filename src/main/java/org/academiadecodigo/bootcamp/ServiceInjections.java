8cea072976f5c29c4dbe29b929718c87b5c6ec54
Miguel Ferreira <miguel.ferreira89@gmail.com>
2017-07-28 01:42:44 +0100

    bug fixed

--- src/main/java/org/academiadecodigo/bootcamp/ServiceInjections.java
+++ src/main/java/org/academiadecodigo/bootcamp/ServiceInjections.java
@@ -18,7 +18,7 @@ import org.hibernate.Hibernate;
 public class ServiceInjections {
 
 
-    public void start(){
+    public void start() {
 
         AnswerDao answerDao = new HibernateAnswerDao();
         KeyWordDao keyWordDao = new HibernateKeyWordDao();
@@ -26,33 +26,35 @@ public class ServiceInjections {
 
 
         ServiceRegistry serviceRegistry = ServiceRegistry.getInstance();
-        serviceRegistry.addService(new AnswerKeyService(keyWordDao,answerDao,transactionManager));
+        serviceRegistry.addService(new AnswerKeyService(keyWordDao, answerDao, transactionManager));
 
-        KeyWordService keyWordService = new KeyWordService(keyWordDao,transactionManager);
+        KeyWordService keyWordService = new KeyWordService(keyWordDao, transactionManager);
         KeyWord keyWord = new KeyWord("dasda");
+        KeyWord keyWord1 = new KeyWord("preguiç");
         keyWordService.addKeyWord(keyWord);
 
-        AnswerService answerService = new AnswerService(answerDao,transactionManager);
+
+        AnswerService answerService = new AnswerService(answerDao, transactionManager);
         answerService.addAnswer(new Answer("Isso são pormenores de implementação.", keyWord));
         answerService.addAnswer(new Answer("Abraça a mudança!", keyWord));
         answerService.addAnswer(new Answer("Come JAVAs com chouriço, que isso passa.", keyWord));
         answerService.addAnswer(new Answer("O que quer que tenhas perguntado...a resposta provavelmente é: referência", keyWord));
         answerService.addAnswer(new Answer("Uma coisa é uma coisa, outra coisa é outra coisa", keyWord));
-        answerService.addAnswer(new Answer("Queres ser meu amigo?", keyWord));
+        answerService.addAnswer(new Answer("Se estiveres com dúvidas, desenha uma heap!", keyWord));
         answerService.addAnswer(new Answer("Não sei se estou a perceber bem a tua pergunta...", keyWord));
         answerService.addAnswer(new Answer("Importas-te de ser um bocadinho mais específico?", keyWord));
-        answerService.addAnswer(new Answer("Steve Jobs, faz-me um filho", keyWord));
+        answerService.addAnswer(new Answer("", keyWord));
         answerService.addAnswer(new Answer("Sabes o que é que era fixe...?", keyWord));
-
+        answerService.addAnswer(new Answer("Um programador é um preguiçoso criativo", keyWord1));
         serviceRegistry.addService(keyWordService);
         serviceRegistry.addService(answerService);
     }
 
-    public void load(){
+    public void load() {
         HibernateSessionManager.getSession();
     }
 
-    public void stop(){
+    public void stop() {
         HibernateSessionManager.close();
     }
 
