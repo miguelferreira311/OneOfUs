85869cf022138898775a8182001a8f673e4dcb48
Audrey Lopes <audrey_lopes@hotmail.com>
2017-07-28 08:44:11 +0100

    challenge controller

---
+++ src/main/java/org/academiadecodigo/bootcamp/controller/ChallengeController.java
@@ -0,0 +1,236 @@
+package org.academiadecodigo.bootcamp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import org.academiadecodigo.bootcamp.Navigation;
+
+import java.util.Map;
+
+/**
+ * Created by codecadet on 28/07/17.
+ */
+public class ChallengeController implements Controller {
+
+    private int counter = 1;
+    private Map<Integer, RadioButton> solutionsMap;
+    private Map<String, String[]> questionsMap;
+    private boolean isCorrect;
+
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Label question;
+
+    @FXML
+    private RadioButton a;
+
+    @FXML
+    private RadioButton b;
+
+    @FXML
+    private RadioButton c;
+
+    @FXML
+    private RadioButton d;
+
+    @FXML
+    private RadioButton e;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    void aClicked(ActionEvent event) {
+        if (counter == 1) {
+            isCorrect = true;
+
+        } else {
+            isCorrect = false;
+        }
+
+    }
+
+    @FXML
+    void bClicked(ActionEvent event) {
+        if (counter == 2 || counter == 3 || counter == 4 || counter == 5) {
+            isCorrect = true;
+
+        } else {
+            isCorrect = false;
+        }
+
+    }
+
+    @FXML
+    void backPress(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cClicked(ActionEvent event) {
+        if (counter == 6) {
+            isCorrect = true;
+
+        } else {
+            isCorrect = false;
+        }
+
+    }
+
+    @FXML
+    void dClicked(ActionEvent event) {
+        isCorrect = false;
+    }
+
+    @FXML
+    void eClicked(ActionEvent event) {
+        if (counter == 7) {
+            isCorrect = true;
+
+        } else {
+            isCorrect = false;
+        }
+    }
+
+    @FXML
+    void sendPress(ActionEvent event) {
+
+
+        if (isCorrect) {
+
+            switch (counter) {
+                case 1:
+                    question2();
+                    isCorrect = false;
+                    counter++;
+                    break;
+                case 2:
+                    question3();
+                    isCorrect = false;
+                    counter++;
+                    break;
+                case 3:
+                    question4();
+                    isCorrect = false;
+                    counter++;
+                    break;
+                case 4:
+                    question5();
+                    isCorrect = false;
+                    counter++;
+                    break;
+                case 5:
+                    question6();
+                    isCorrect = false;
+                    counter++;
+                    break;
+                case 6:
+                    question7();
+                    isCorrect = false;
+                    counter++;
+                    break;
+                case 7:
+                    victory();
+                    break;
+                default:
+                    System.exit(1);
+            }
+        }
+    }
+
+    private void victory() {
+
+
+
+    }
+
+    private void question7() {
+        question.setText("7 - Qual das seguintes opções completa de forma correcta a seguinte frase: Em Java, os construtores...");
+        a.setText("não podem lançar exceções");
+        b.setText("não têm acesso aos atributos da classe base");
+        c.setText("não podem alterar os valores dos atributos do objecto a criar");
+        d.setText("nunca podem ser privados");
+        e.setText("podem chamar outros construtores da mesma classe");
+    }
+
+    private void question6() {
+        question.setText("6 - A definição de uma classe por agregação de objectos de outras classes ou suas colecções designa-se por:");
+        a.setText("upcasting");
+        b.setText("abstracção");
+        c.setText("composição");
+        d.setText("herança");
+        e.setText("extensão");
+    }
+
+    private void question5() {
+        question.setText("5 - O padrão de desenho Observer...");
+        a.setText("representa uma operação a ser realizada sobre os elementos de uma estrutura de objectos");
+        b.setText("gerir as dependências de outros objectos relativamente ao estado de um objecto");
+        c.setText("permite abstrair a criação de famílias de objectos para uma aplicação");
+        d.setText("permite tratar famílias de objectos e seus grupos indiscriminadamente");
+        e.setText("permite que o comportamento de um objeto mude quando o seu estado muda");
+    }
+
+    private void question4() {
+        question.setText("4 - Em Java, um método declarado private…");
+        a.setText("não pode chamar outros métodos");
+        b.setText("não pode ser overridden");
+        c.setText("não pode ser declarado static");
+        d.setText("não pode ser usado por construtores");
+        e.setText("pode ser acedido por classes da mesma package");
+    }
+
+    private void question3() {
+        question.setText("3 - Uma função recursiva distingue-se por ser: ");
+        a.setText("Uma função com recursos de memória ilimitados");
+        b.setText("Uma função definida por um caso base e uma chamada a si própria");
+        c.setText("Uma função com um número variável de argumentos de entrada");
+        d.setText("Uma função que executa uma tarefa iterativamente");
+        e.setText("Nenhuma das anteriores.");
+    }
+
+    private void question2() {
+        question.setText("2 - Em Java, uma interface...");
+        a.setText("implementa métodos");
+        b.setText("tem apenas membros públicos");
+        c.setText("pode definir construtores, desde que sejam públicos");
+        d.setText("não pode ser privada");
+        e.setText("não pode conter classes internas");
+    }
+
+    public void initialize() {
+
+        addSolutions();
+
+    }
+
+
+    private void addSolutions() {
+        solutionsMap.put(1, a);
+        solutionsMap.put(2, b);
+        solutionsMap.put(3, b);
+        solutionsMap.put(4, b);
+        solutionsMap.put(5, b);
+        solutionsMap.put(6, c);
+        solutionsMap.put(7, e);
+
+    }
+
+
+    private void question1() {
+        question.setText("1 - Em Java, qual das seguintes frases está correcta?");
+        a.setText("Uma classe pode estender uma única classe");
+        b.setText("Uma classe pode estender várias classes");
+        c.setText("Uma classe só pode implementar uma única interface");
+        d.setText("Uma classe não pode simultaneamente estender uma classe e implementar uma interface");
+        e.setText("Uma classe não pode simultaneamente estender uma classe e implementar mais do que uma interface");
+    }
+}
+
+
+
